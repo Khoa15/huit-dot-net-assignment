dfece6c562671ee3e8b6161ee5354893c8dad66f
ThaoTo2806 <thaonguyen28062003@gmail.com>
2023-09-15 12:30:39 +0700

    update1

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,203 +3,78 @@
 USE TaiLieuSo
 GO
 
--- Tạo bảng Thư mục (Folder)
+-- Tạo bảng Folder với liên kết đến chính nó
 CREATE TABLE Folder (
-    FolderID INT PRIMARY KEY,
-    FolderName VARCHAR(255),
-    Description TEXT,
-    CreatedDate DATE,
-    CreatedBy VARCHAR(50)
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    description TEXT,
+    created_date DATE,
+    created_by VARCHAR(50),
+    parent_id INT, -- Thêm liên kết với chính nó
+    FOREIGN KEY (parent_id) REFERENCES Folder(id)
 );
 GO
 
--- Tạo bảng Tài liệu (Document)
+-- Tạo bảng Document với các thay đổi
 CREATE TABLE Document (
-    DocumentID INT PRIMARY KEY,
-    DocumentName VARCHAR(255),
-    Description TEXT,
-    FilePath VARCHAR(255),
-    FolderID INT,
-    CreatedDate DATE,
-    CreatedBy VARCHAR(50),
-    FOREIGN KEY (FolderID) REFERENCES Folder(FolderID)
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    description TEXT,
+    file_path VARCHAR(255),
+    folder_id INT,
+    created_date DATE,
+    created_by_user_access_id INT, -- Khóa ngoại tới bảng UserAccess
+    link_to_image VARCHAR(255), -- Thêm cột Link ảnh (local hoặc URL)
+    document_type VARCHAR(50), -- Loại tài liệu
+    document_status INT, -- Trạng thái tài liệu
+    author_id INT, -- Khóa ngoại tới bảng Author
+    FOREIGN KEY (folder_id) REFERENCES Folder(id),
+    --FOREIGN KEY (created_by_user_access_id) REFERENCES UserAccess(id), -- Khóa ngoại tới bảng UserAccess
+    --FOREIGN KEY (author_id) REFERENCES Author(id)
 );
 GO
 
--- Tạo bảng Chính sách Phân quyền (AccessPolicy)
-CREATE TABLE AccessPolicy (
-    PolicyID INT PRIMARY KEY,
-    PolicyName VARCHAR(255),
-    Description TEXT,
-    AccessRights VARCHAR(255) -- Có thể sử dụng một kiểu dữ liệu phù hợp cho quyền truy cập
-);
-GO
-
--- Tạo bảng Phân quyền Người dùng (UserAccess)
+-- Tạo bảng UserAccess với các thay đổi
 CREATE TABLE UserAccess (
-    UserAccessID INT PRIMARY KEY,
-    UserID INT, -- Đặt các trường liên quan đến người dùng ở đây
-    PolicyID INT,
-    DocumentID INT,
-    FolderID INT,
-    FOREIGN KEY (PolicyID) REFERENCES AccessPolicy(PolicyID),
-    FOREIGN KEY (DocumentID) REFERENCES Document(DocumentID),
-    FOREIGN KEY (FolderID) REFERENCES Folder(FolderID)
+    id INT PRIMARY KEY,
+    policy_id INT,
+    document_id INT,
+    folder_id INT,
+    display INT,
+    read_limit INT,
+    read_full INT,
+    download INT,
+    page_read INT,
+    page_download INT,
+    user_type_id INT, -- Liên kết với bảng UserType
+    FOREIGN KEY (document_id) REFERENCES Document(id),
+    FOREIGN KEY (folder_id) REFERENCES Folder(id)
 );
 GO
 
--- Tạo trigger 
--- Procedure để thêm mới một thư mục
-CREATE PROCEDURE AddFolder
-    @FolderID INT,
-    @FolderName VARCHAR(255),
-    @Description TEXT,
-    @CreatedDate DATE,
-    @CreatedBy VARCHAR(50)
-AS
-BEGIN
-    INSERT INTO Folder (FolderID, FolderName, Description, CreatedDate, CreatedBy)
-    VALUES (@FolderID, @FolderName, @Description, @CreatedDate, @CreatedBy);
-END;
+-- Tạo bảng Author
+CREATE TABLE Author (
+    id INT PRIMARY KEY,
+    author_name VARCHAR(255),
+    description TEXT
+);
 GO
 
--- Procedure để xóa một thư mục bằng FolderID
-CREATE PROCEDURE DeleteFolder
-    @FolderID INT
-AS
-BEGIN
-    DELETE FROM Folder WHERE FolderID = @FolderID;
-END;
+-- Khôi phục lại bảng DocumentIndex
+CREATE TABLE DocumentIndex (
+    index_id INT PRIMARY KEY,
+    title VARCHAR(255),
+    document_id INT,
+    page_number INT,
+    parent_index_id INT, -- Thêm cột parent_index_id làm khóa ngoại
+    author_id INT, -- Thêm cột author_id làm khóa ngoại
+    FOREIGN KEY (document_id) REFERENCES Document(id),
+    FOREIGN KEY (parent_index_id) REFERENCES DocumentIndex(index_id), -- Liên kết với chính nó
+    FOREIGN KEY (author_id) REFERENCES Author(id)
+);
 GO
 
--- Procedure để sửa thông tin của một thư mục bằng FolderID
-CREATE PROCEDURE UpdateFolder
-    @FolderID INT,
-    @FolderName VARCHAR(255),
-    @Description TEXT,
-    @CreatedDate DATE,
-    @CreatedBy VARCHAR(50)
-AS
-BEGIN
-    UPDATE Folder
-    SET FolderName = @FolderName, Description = @Description, CreatedDate = @CreatedDate, CreatedBy = @CreatedBy
-    WHERE FolderID = @FolderID;
-END;
-GO
-
--- Procedure để thêm mới một tài liệu
-CREATE PROCEDURE AddDocument
-    @DocumentID INT,
-    @DocumentName VARCHAR(255),
-    @Description TEXT,
-    @FilePath VARCHAR(255),
-    @FolderID INT,
-    @CreatedDate DATE,
-    @CreatedBy VARCHAR(50)
-AS
-BEGIN
-    INSERT INTO Document (DocumentID, DocumentName, Description, FilePath, FolderID, CreatedDate, CreatedBy)
-    VALUES (@DocumentID, @DocumentName, @Description, @FilePath, @FolderID, @CreatedDate, @CreatedBy);
-END;
-GO
-
--- Procedure để xóa một tài liệu bằng DocumentID
-CREATE PROCEDURE DeleteDocument
-    @DocumentID INT
-AS
-BEGIN
-    DELETE FROM Document WHERE DocumentID = @DocumentID;
-END;
-GO
-
--- Procedure để sửa thông tin của một tài liệu bằng DocumentID
-CREATE PROCEDURE UpdateDocument
-    @DocumentID INT,
-    @DocumentName VARCHAR(255),
-    @Description TEXT,
-    @FilePath VARCHAR(255),
-    @FolderID INT,
-    @CreatedDate DATE,
-    @CreatedBy VARCHAR(50)
-AS
-BEGIN
-    UPDATE Document
-    SET DocumentName = @DocumentName, Description = @Description, FilePath = @FilePath,
-        FolderID = @FolderID, CreatedDate = @CreatedDate, CreatedBy = @CreatedBy
-    WHERE DocumentID = @DocumentID;
-END;
-GO
-
--- Procedure để thêm mới một chính sách phân quyền
-CREATE PROCEDURE AddAccessPolicy
-    @PolicyID INT,
-    @PolicyName VARCHAR(255),
-    @Description TEXT,
-    @AccessRights VARCHAR(255)
-AS
-BEGIN
-    INSERT INTO AccessPolicy (PolicyID, PolicyName, Description, AccessRights)
-    VALUES (@PolicyID, @PolicyName, @Description, @AccessRights);
-END;
-GO
-
--- Procedure để xóa một chính sách phân quyền bằng PolicyID
-CREATE PROCEDURE DeleteAccessPolicy
-    @PolicyID INT
-AS
-BEGIN
-    DELETE FROM AccessPolicy WHERE PolicyID = @PolicyID;
-END;
-GO
-
--- Procedure để sửa thông tin của một chính sách phân quyền bằng PolicyID
-CREATE PROCEDURE UpdateAccessPolicy
-    @PolicyID INT,
-    @PolicyName VARCHAR(255),
-    @Description TEXT,
-    @AccessRights VARCHAR(255)
-AS
-BEGIN
-    UPDATE AccessPolicy
-    SET PolicyName = @PolicyName, Description = @Description, AccessRights = @AccessRights
-    WHERE PolicyID = @PolicyID;
-END;
-GO
-
--- Procedure để thêm mới một quyền truy cập của người dùng
-CREATE PROCEDURE AddUserAccess
-    @UserAccessID INT,
-    @UserID INT,
-    @PolicyID INT,
-    @DocumentID INT,
-    @FolderID INT
-AS
-BEGIN
-    INSERT INTO UserAccess (UserAccessID, UserID, PolicyID, DocumentID, FolderID)
-    VALUES (@UserAccessID, @UserID, @PolicyID, @DocumentID, @FolderID);
-END;
-GO
-
--- Procedure để xóa quyền truy cập của người dùng bằng UserAccessID
-CREATE PROCEDURE DeleteUserAccess
-    @UserAccessID INT
-AS
-BEGIN
-    DELETE FROM UserAccess WHERE UserAccessID = @UserAccessID;
-END;
-GO
-
--- Procedure để sửa thông tin quyền truy cập của người dùng bằng UserAccessID
-CREATE PROCEDURE UpdateUserAccess
-    @UserAccessID INT,
-    @UserID INT,
-    @PolicyID INT,
-    @DocumentID INT,
-    @FolderID INT
-AS
-BEGIN
-    UPDATE UserAccess
-    SET UserID = @UserID, PolicyID = @PolicyID, DocumentID = @DocumentID, FolderID = @FolderID
-    WHERE UserAccessID = @UserAccessID;
-END;
-GO
+--FOREIGN KEY (created_by_user_access_id) REFERENCES UserAccess(id), -- Khóa ngoại tới bảng UserAccess
+    --FOREIGN KEY (author_id) REFERENCES Author(id)
+ALTER TABLE Document ADD FOREIGN KEY (created_by_user_access_id) REFERENCES UserAccess(id)
+ALTER TABLE Document ADD FOREIGN KEY (author_id) REFERENCES Author(id)
