9a31bb8947b29ec167d3670780e917f8d4f2b461
Khoa15 <nguyentrongdangkhoa15@gmail.com>
2023-10-04 09:40:21 +0700

    id

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,17 +1,21 @@
-﻿
+﻿use master
+go
+create database TaiLieuSo
+go
 USE TaiLieuSo
 GO
-
+GO
 -- Tạo bảng Folder với liên kết đến chính nó
 CREATE TABLE Folder (
-    id CHAR(10),
+	ID int IDENTITY(1,1),
+    name_id VARCHAR(10),
     name NVARCHAR(255),
     description TEXT,
     created_date DATE,
-    created_by NVARCHAR(255),
-    parent_id NVARCHAR(255), -- Thêm liên kết với chính nó
-	CONSTRAINT pk_Folder PRIMARY KEY (name),
-    CONSTRAINT fk_Folder FOREIGN KEY (parent_id) REFERENCES Folder(name)
+    created_by NVARCHAR(255), -- foregin key?
+    parent_id int, -- Thêm liên kết với chính nó
+	CONSTRAINT PK_FOLDER PRIMARY KEY (id),
+    CONSTRAINT FK_FOLDER FOREIGN KEY (parent_id) REFERENCES Folder(id)
 );
 GO
 
@@ -21,15 +25,15 @@ CREATE TABLE Document (
     name NVARCHAR(255),
     description TEXT,
     file_path VARCHAR(255),
-    folder_id NVARCHAR(255),
+    folder_id int,
     created_date DATE,
     created_by_user_access_id INT, -- Khóa ngoại tới bảng UserAccess
     link_to_image VARCHAR(255), -- Thêm cột Link ảnh (local hoặc URL)
     document_type NVARCHAR(255), -- Loại tài liệu
     document_status INT, -- Trạng thái tài liệu
-    author_id CHAR(10), -- Khóa ngoại tới bảng Author
-	CONSTRAINT pk_Document PRIMARY KEY (id),
-    CONSTRAINT fk_Document_Folder FOREIGN KEY (folder_id) REFERENCES Folder(name),
+    author_id INT, -- Khóa ngoại tới bảng Author
+	CONSTRAINT PK_DOCUMENT PRIMARY KEY (id),
+    CONSTRAINT FK_DOCUMENT_FOLDER FOREIGN KEY (folder_id) REFERENCES Folder(id),
     --FOREIGN KEY (created_by_user_access_id) REFERENCES UserAccess(id), -- Khóa ngoại tới bảng UserAccess
     --FOREIGN KEY (author_id) REFERENCES Author(id)
 );
@@ -48,7 +52,7 @@ CREATE TABLE UserAccess (
     page_read INT,
     page_download INT,
     user_type_id INT,
-	CONSTRAINT pk_UserAccess PRIMARY KEY (id)-- Liên kết với bảng UserType
+	CONSTRAINT PK_USERACCESS PRIMARY KEY (id)-- Liên kết với bảng UserType
     --FOREIGN KEY (document_id) REFERENCES Document(id),
     --FOREIGN KEY (folder_id) REFERENCES Folder(id)
 );
@@ -59,7 +63,7 @@ CREATE TABLE Author (
     id INT IDENTITY(1,1),
     author_name NVARCHAR(255),
     description TEXT,
-	CONSTRAINT pk_Author PRIMARY KEY (id)
+	CONSTRAINT PK_AUTHOR PRIMARY KEY (id)
 );
 GO
 
@@ -71,28 +75,32 @@ CREATE TABLE DocumentIndex (
     page_number INT,
     parent_index_id INT, -- Thêm cột parent_index_id làm khóa ngoại
     author_id INT, -- Thêm cột author_id làm khóa ngoại
-	CONSTRAINT pk_DocumentIndex PRIMARY KEY (index_id),
-    CONSTRAINT fk_DocumentIndex_Document FOREIGN KEY (document_id) REFERENCES Document(id),
-    CONSTRAINT fk_DocumentIndex FOREIGN KEY (parent_index_id) REFERENCES DocumentIndex(index_id), -- Liên kết với chính nó
-    CONSTRAINT fk_DocumentIndex_Author FOREIGN KEY (author_id) REFERENCES Author(id)
+	CONSTRAINT PK_DOCUMENTINDEX PRIMARY KEY (index_id),
+    CONSTRAINT FK_DOCUMENTINDEX_DOCUMENT FOREIGN KEY (document_id) REFERENCES Document(id),
+    CONSTRAINT FK_DOCUMENTINDEX FOREIGN KEY (parent_index_id) REFERENCES DocumentIndex(index_id), -- Liên kết với chính nó
+    CONSTRAINT FK_DOCUMENTINDEX_AUTHOR FOREIGN KEY (author_id) REFERENCES Author(id)
 );
 GO
 
-ALTER TABLE Document ADD CONSTRAINT fk_Document_UserAccess FOREIGN KEY (created_by_user_access_id) REFERENCES UserAccess(id)
+ALTER TABLE Document ADD CONSTRAINT FK_DOCUMENT_USERACCESS FOREIGN KEY (created_by_user_access_id) REFERENCES UserAccess(id)
 GO
-ALTER TABLE Document ADD CONSTRAINT fk_Document_Author FOREIGN KEY (author_id) REFERENCES Author(id)
+ALTER TABLE Document ADD CONSTRAINT FK_DOCUMENT_AUTHOR FOREIGN KEY (author_id) REFERENCES Author(id)
 GO
+SET IDENTITY_INSERT Folder ON;
 
-INSERT INTO Folder (id, name, description, created_date, created_by, parent_id)
+INSERT INTO Folder (ID, name_id, name, description, created_date, created_by, parent_id)
 VALUES
-    ('DBH', N'Đã ban hành', NULL, '2023-09-06', 'admin', NULL),
-    ('CBH', N'Chưa ban hành', NULL, '2023-09-06', 'admin', NULL),
-    ('CBM', N'Chưa biên mục', NULL, '2023-09-06', 'admin', NULL),
-    ('CN', N'Công nghiệp', NULL, '2023-09-06', 'admin', NULL),
-    ('CN1', N'Công nghiệp', NULL, '2023-09-06', 'admin', N'Công nghiệp'),
-    ('CN2', N'Công nghiệp hóa chất', NULL, '2023-09-06', 'admin', N'Công nghiệp'),
-    ('CN3', N'Công nghiệp sx hàng tiêu dùng', NULL, '2023-09-06', 'admin', N'Công nghiệp'),
-    ('CN4', N'Công nghiệp thực phẩm', NULL, '2023-09-06', 'admin', N'Công nghiệp');
+    (1, 'DBH', N'Đã ban hành', NULL, '2023-09-06', 'admin', NULL),
+    (2, 'CBH', N'Chưa ban hành', NULL, '2023-09-06', 'admin', NULL),
+    (3, 'CBM', N'Chưa biên mục', NULL, '2023-09-06', 'admin', NULL),
+    (4, 'CN', N'Công nghiệp', NULL, '2023-09-06', 'admin', NULL),
+    (5, 'CN1', N'Công nghiệp', NULL, '2023-09-06', 'admin', 4),
+    (6, 'CN2', N'Công nghiệp hóa chất', NULL, '2023-09-06', 'admin', 4),
+    (7, 'CN3', N'Công nghiệp sx hàng tiêu dùng', NULL, '2023-09-06', 'admin', 4),
+    (8, 'CN4', N'Công nghiệp thực phẩm', NULL, '2023-09-06', 'admin', 4);
+
+SET IDENTITY_INSERT Folder OFF;
+
 GO
 
 --SELECT * FROM Folder
