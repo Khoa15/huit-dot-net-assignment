ad0b6a995973b68631502ff7d89983ff345978a5
Khoa15 <nguyentrongdangkhoa15@gmail.com>
2023-10-29 10:58:29 +0700

    init dll

---
+++ DigitalDocumentary/DLL/AuthorDLL.cs
@@ -0,0 +1,24 @@
+﻿using DigitalDocumentary.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalDocumentary.DLL
+{
+    internal class AuthorDLL
+    {
+        private List<AuthorDTO> authors = new List<AuthorDTO>();
+        public AuthorDLL()
+        {
+
+        }
+        public List<AuthorDTO> Load()
+        {
+            authors.Clear();
+
+            return authors;
+        }
+    }
+}
---
+++ DigitalDocumentary/DTO/DatabaseContextDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalDocumentary.DTO
+{
+    internal class DatabaseContextDTO
+    {
+        private string strCon = "";
+        private SqlConnection sqlCon;
+        public DatabaseContextDTO() { }
+        public DatabaseContextDTO(string sqlCon)
+        {
+            this.strCon = sqlCon;
+        }
+
+        public SqlConnection Sql { get => sqlCon; set => sqlCon = value; }
+    }
+}
